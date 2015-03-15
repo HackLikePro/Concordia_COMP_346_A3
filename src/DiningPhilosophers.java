cfe0018fa00356c9c42a33609fbd7c7131552a13
iantab <tablate.ian@gmail.com>
2015-03-15 17:17:46 -0400

    Step 3 added
    monitor works good so i just did step 3, you can test the stuff if you
    want

--- src/DiningPhilosophers.java
+++ src/DiningPhilosophers.java
@@ -4,6 +4,8 @@
  *
  * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
  */
+import java.util.Scanner;
+
 public class DiningPhilosophers
 {
 	/*
@@ -50,43 +52,64 @@ public class DiningPhilosophers
 			 * Should be settable from the command line
 			 * or the default if no arguments supplied.
 			 */
+			
 			int iPhilosophers = DEFAULT_NUMBER_OF_PHILOSOPHERS;
-
-			// Make the monitor aware of how many philosophers there are
-			soMonitor = new Monitor(iPhilosophers);
-
-			// Space for all the philosophers
 			
-			Philosopher aoPhilosophers[] = new Philosopher[iPhilosophers];
-
-			System.out.println
-			(
-				iPhilosophers +
-				" philosopher(s) came in for a dinner."
-			);
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter number of Philosophers: ");
 			
-			// Let 'em sit down
-			for(int j = 0; j < iPhilosophers; j++)
+			String philoInput = input.nextLine();
+			input.close();
+			
+			if(philoInput.length() != 0)
 			{
-				aoPhilosophers[j] = new Philosopher();
-				aoPhilosophers[j].start();
+				double temp = Double.parseDouble(philoInput);
+				iPhilosophers = (int) temp;
 			}
-
 			
-
-			// Main waits for all its children to die...
-			// I mean, philosophers to finish their dinner.
-			for(int j = 0; j < iPhilosophers; j++)
-				aoPhilosophers[j].join();
-
-			System.out.println("All philosophers have left. System terminates normally.");
+			if(iPhilosophers < 0)
+				System.out.println(iPhilosophers + " is a negative number.");
+			else
+			{
+				// Make the monitor aware of how many philosophers there are
+				soMonitor = new Monitor(iPhilosophers);
+	
+				// Space for all the philosophers
+				
+				Philosopher aoPhilosophers[] = new Philosopher[iPhilosophers];
+	
+				System.out.println
+				(
+					iPhilosophers +
+					" philosopher(s) came in for a dinner."
+				);
+				
+				// Let 'em sit down
+				for(int j = 0; j < iPhilosophers; j++)
+				{
+					aoPhilosophers[j] = new Philosopher();
+					aoPhilosophers[j].start();
+				}
+	
+				
+	
+				// Main waits for all its children to die...
+				// I mean, philosophers to finish their dinner.
+				for(int j = 0; j < iPhilosophers; j++)
+					aoPhilosophers[j].join();
+	
+				System.out.println("All philosophers have left. System terminates normally.");
+				
+				}
 		}
+		
 		catch(InterruptedException e)
 		{
 			System.err.println("main():");
 			reportException(e);
 			System.exit(1);
 		}
+		
 	} // main()
 
 	/**
